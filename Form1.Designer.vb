0158d2a9a062653592310c325ec4408e90ae198b
LunayderKater <d.caddies@outlook.de>
2023-08-27 21:41:06 +0200

    Update GUI & Functions

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,157 @@ Partial Class Form1
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.startprofile = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Items.AddRange(New Object() {""})
+        Me.ListBox1.Location = New System.Drawing.Point(15, 14)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(351, 212)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(374, 14)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(168, 72)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Play"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(374, 94)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(168, 36)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Create Profile"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Transparent
+        Me.Button3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(374, 138)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(168, 36)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Delete Profile"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(388, 184)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(138, 36)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Made by" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "LunayderKater"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer1
+        '
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Silver
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Location = New System.Drawing.Point(93, 67)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(369, 114)
+        Me.Panel1.TabIndex = 5
+        Me.Panel1.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(0, 16)
+        Me.Label2.MinimumSize = New System.Drawing.Size(369, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(369, 80)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Preparing Valley Switcher" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "please wait"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer2
+        '
+        '
+        'startprofile
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.Color.Black
+        Me.BackgroundImage = Global.Valley_Switcher.My.Resources.Resources.bg
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(551, 246)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.DoubleBuffered = True
+        Me.Enabled = False
+        Me.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Valley Switcher"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents startprofile As Timer
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,93 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+    Dim selpro As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.Clear()
+        Button1.BackColor = Color.FromArgb(128, 0, 255, 0)
+        Button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(200, 0, 255, 0)
+        Button2.BackColor = Color.FromArgb(128, 200, 200, 0)
+        Button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(200, 200, 200, 0)
+        Button3.BackColor = Color.FromArgb(128, 150, 0, 0)
+        Button3.FlatAppearance.MouseOverBackColor = Color.FromArgb(200, 150, 0, 0)
+
+        If My.Settings.path = Nothing Then
+            Timer1.Start()
+        Else
+            Try
+                For Each folder In IO.Directory.GetDirectories(My.Settings.path & "\valley-switcher\")
+                    ListBox1.Items.Add(folder.ToString.Replace(My.Settings.path & "\valley-switcher\", ""))
+                Next
+                Me.Enabled = True
+            Catch ex As Exception
+                Timer1.Start()
+            End Try
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Stop()
+        Dim fbro As New FolderBrowserDialog
+        MsgBox("Please select the Stardew Valley Folder.", MsgBoxStyle.OkOnly, "Valley Switcher")
+anotherone:
+        If fbro.ShowDialog() = DialogResult.OK Then
+            If IO.Directory.Exists(fbro.SelectedPath & "\mods\") = True Then
+                Panel1.Visible = True
+                IO.Directory.CreateDirectory(fbro.SelectedPath & "\valley-switcher\")
+                IO.Directory.CreateDirectory(fbro.SelectedPath & "\valley-switcher\No Mods\")
+                My.Settings.path = fbro.SelectedPath
+                My.Settings.Save()
+                Timer2.Start()
+            Else
+                Panel1.Visible = False
+                MsgBox("Cant find the mods folder in " & fbro.SelectedPath & " please check if the correct folder was choosen and smapi is installed!", MsgBoxStyle.OkOnly, "Valley Switcher")
+                GoTo anotherone
+            End If
+        Else
+            Application.Exit()
+        End If
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        My.Settings.Reset()
+        My.Settings.Save()
+        Application.Exit()
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Timer2.Stop()
+        Try
+            My.Computer.FileSystem.CopyDirectory(My.Settings.path & "\mods\", My.Settings.path & "\valley-switcher\All Mods\")
+        Catch ex As Exception
+        End Try
+        Me.Enabled = True
+        Panel1.Visible = False
+        For Each folder In IO.Directory.GetDirectories(My.Settings.path & "\valley-switcher\")
+            ListBox1.Items.Add(folder.ToString.Replace(My.Settings.path & "\valley-switcher\", ""))
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If selpro = Nothing Then
+        Else
+            Label2.Text = "Starting Profile: " & selpro & vbCrLf & "be starting soon!"
+            Panel1.Visible = True
+            Me.Enabled = False
+            startprofile.Start()
+        End If
+    End Sub
+
+    Private Sub ListBox1_SelectedValueChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedValueChanged
+        selpro = ListBox1.SelectedItem.ToString
+    End Sub
+
+    Private Sub startprofile_Tick(sender As Object, e As EventArgs) Handles startprofile.Tick
+        startprofile.Stop()
+        IO.Directory.Delete(My.Settings.path & "\mods\", True)
+            My.Computer.FileSystem.CopyDirectory(My.Settings.path & "\valley-switcher\" & selpro & "\", My.Settings.path & "\mods\")
+            Process.Start(My.Settings.path & "\StardewModdingAPI.exe")
+        Application.Exit()
+    End Sub
 End Class
